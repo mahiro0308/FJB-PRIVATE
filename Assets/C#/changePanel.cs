4d5d8307ded055b0f82cae66507d1846b423b74e
mahiro0308 <candylook0308@gmail.com>
2025-01-02 02:33:24 +0900

    latest

---
+++ Assets/C#/changePanel.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changePanel : MonoBehaviour
+{
+    public GameObject informationBtn;
+    public GameObject friendlistBtn;
+    public GameObject homeBtn;
+    public GameObject arBtn;
+    public GameObject chatBtn;
+    public GameObject shopBtn;
+
+    public GameObject informationpanel;
+    public GameObject friendlistpanel;
+    public GameObject homepanel;
+    public GameObject arpanel;
+    public GameObject chatpanel;
+    public GameObject shoppanel;
+
+    // 初期化処理
+    public void Start()
+    {
+        // 最初に表示するパネルのみアクティブに設定
+        informationpanel.SetActive(false);
+        friendlistpanel.SetActive(false);
+        homepanel.SetActive(true);  // ホームパネルをデフォルトでアクティブに
+        arpanel.SetActive(false);
+        chatpanel.SetActive(false);
+        shoppanel.SetActive(false);
+    }
+
+    // パネルを切り替えるメソッド
+    public void changepanel(string panelName)
+    {
+        // すべてのパネルを非アクティブにする
+        informationpanel.SetActive(false);
+        friendlistpanel.SetActive(false);
+        homepanel.SetActive(false);
+        arpanel.SetActive(false);
+        chatpanel.SetActive(false);
+        shoppanel.SetActive(false);
+
+        // 指定されたパネルのみアクティブにする
+        switch (panelName)
+        {
+            case "information":
+                informationpanel.SetActive(true);
+                break;
+            case "friendlist":
+                friendlistpanel.SetActive(true);
+                break;
+            case "home":
+                homepanel.SetActive(true);
+                break;
+            case "ar":
+                arpanel.SetActive(true);
+                break;
+            case "chat":
+                chatpanel.SetActive(true);
+                break;
+            case "shop":
+                shoppanel.SetActive(true);
+                break;
+            default:
+                Debug.LogError("Invalid panel name: " + panelName);
+                break;
+        }
+    }
+}
